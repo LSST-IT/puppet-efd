6907c48a6db75c35d5ab56b48000b3eb8eb084d7
Andres Villalobos <aevr007@gmail.com>
2019-05-30 17:33:41 -0400

    missing user
    Giving the rpms are considering salmgr as the user it will execute the code, the user itself needs to be created.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,4 +3,16 @@ class efd{
   include efd::efd_writers
   include efd::efd_mysql
   include efd::efd_influxdb
+
+  #This is the user to be used within the EFD Writers
+  user{ 'salmgr':
+    ensure     => 'present',
+    uid        => '501' ,
+    gid        => '500',
+    home       => '/home/salmgr',
+    managehome => true,
+    require    => Group['lsst'],
+    password   => lookup('salmgr_pwd'),
+  }
+
 }
